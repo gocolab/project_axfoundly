052a934c6c6e1c304a945554104e1e1d08ee9444
Sanghun Oh <otter.oh@gmail.com>
2026-09-06 12:34:34 +0000

    test: improve playwright test configuration with list reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: 0,
   workers: 1,
+  reporter: [['list']],
   use: {
     baseURL: 'http://localhost:3010',
     trace: 'off',
